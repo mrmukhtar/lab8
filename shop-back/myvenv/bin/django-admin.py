f5af2ea1b900da764b6fc800783ccbe7e954b5b2
Mukhtar <amirkumarmukhtar@gmail.com>
2020-03-28 16:41:01 +0600

    Done!

---
+++ shop-back/myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/daniyarbekulyyerassyl/Documents/WebDevelopment/shop-back/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
